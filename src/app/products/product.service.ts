4bb46a1abc48505055fefb8eab3a8a2aa3011c9a
benmuratseker <benmuratseker@hotmail.com>
2024-11-21 23:37:13 +0100

    not found error handle on service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,31 +1,43 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { Product } from './product';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { catchError, Observable, of, tap, throwError } from "rxjs";
+import { Product } from "./product";
+import { ProductData } from "./product-data";
+import { HttpErrorService } from "../utilities/http-error.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
-  private productsUrl = 'api/products';
+  private productsUrl = "api/productss";
+  private errorService = inject(HttpErrorService);
 
-  //constructor(private http: HttpClient) {} 
+  //constructor(private http: HttpClient) {}
 
   private http = inject(HttpClient);
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-    .pipe(
-      tap(() => console.log('Inc http.get pipeline'))
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(() => console.log("Inc http.get pipeline")),
+      // catchError((err) => {
+      //   console.error(err);
+      //   return of(ProductData.products);
+      // })
+      catchError(err => this.handleError(err))
     );
   }
 
   getProduct(id: number): Observable<Product> {
-    const productUrl = this.productsUrl + '/' + id;
+    const productUrl = this.productsUrl + "/" + id;
 
-    return this.http.get<Product>(productUrl)
-      .pipe(
-        tap(() => console.log('In http.get by id pipeline'))
-      );
+    return this.http
+      .get<Product>(productUrl)
+      .pipe(tap(() => console.log("In http.get by id pipeline")));
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> { //Observable does not emit anything in here
+    const formattedMessage = this.errorService.formatError(err);
+    //return throwError(() => formattedMessage);
+    throw formattedMessage;
   }
 }
